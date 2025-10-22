4bf67eb0ed6ad77dbf813367d65b61175845f506
rfrontis-art <rfrontis@ucsc.edu>
2025-10-21 23:09:16 -0700

    Create ex04.js

---
+++ exercises/ex04.js
@@ -0,0 +1,25 @@
+
+let totalNumber = 0;
+
+
+// add a button titled click me
+
+$("#needy-button").click( function(){
+
+    totalNumber = totalNumber + 1;
+
+let sentence = "I was clicked "; 
+let totalNumbers = sentence + totalNumber;
+   
+    $("#needy-button").html(sentence);
+});
+
+// when it is clicked
+// add one to the total number
+
+// show how many times
+// on the button
+// show "clicked total number times"
+
+
+// a top limit
